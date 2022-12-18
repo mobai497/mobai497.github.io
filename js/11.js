27a830a9b206e4d0bd207135db7ba66c380d18b8
mobai497 <51247771+mobai497@users.noreply.github.com>
2022-12-18 17:44:27 +0800

    Update 11.js

--- js/11.js
+++ js/11.js
@@ -69,6 +69,10 @@ var Index = {
                 return validaresult;
             },
             callback: function () {
+                if (isGetLocationed == false) {
+                    layer.msg('定位获取中，请稍后……');
+                    return false;
+                }
                 $('.save_form').attr('disabled', "disabled");
                 $(".save_form").text("提交中...");
                 HandleFormData();
@@ -215,7 +219,77 @@ function CKTY() {
 }
 
 function HandleFormData() {
-    $("#FaProvinceName")="福建省";
-    $("#FaCityName")="厦门市";
-    $("#FaCountyName")="集美区";
+    var arrData = new Array();
+    //单选框数据组装
+    var radio = parseInt($("#radioCount").val());
+    for (var r = 1; r <= radio; r++) {
+        var sel = $("input[name='radio_" + r + "']:checked");
+        if (sel.length > 0) {
+            var obj = new Object();
+            obj.OptionName = $(sel).attr("data-optionname");
+            obj.SelectId = $(sel).val();
+            obj.TitleId = $(sel).parent(".item").attr("data-tid");
+            obj.OptionType = "0";
+            arrData.push(obj);
+        }
+    }
+
+    //多选框数据组装
+    var chbox = parseInt($("#checkboxCount").val());
+    for (var r = 1; r <= chbox; r++) {
+        var elm = document.getElementsByName("checkbox_" + r);
+        if (elm.length > 0) {
+            var obj = new Object();
+            obj.TitleId = $(elm[0]).parent(".item").attr("data-tid");
+            for (var i = 0; i < elm.length; i++) {
+                if (elm[i].checked) {
+                    if (obj.OptionName) {
+                        obj.OptionName += "、";
+                        obj.SelectId += "、";
+                    }
+                    else {
+                        obj.OptionName = "";
+                        obj.SelectId = "";
+                    }
+                    obj.OptionName += $(elm[i]).attr("data-optionname");
+                    obj.SelectId += $(elm[i]).val();
+                }
+            }
+            if (obj.OptionName) {
+                obj.OptionType = "1";
+                arrData.push(obj);
+            }
+        }
+    }
+    //填空题
+    var blackCount = parseInt($("#blackCount").val());
+    for (var r = 1; r <= blackCount; r++) {
+        var elm = $("input[name='text_" + r + "']");
+        if (elm) {
+            var inputContent = $(elm).val();
+            if (inputContent) {
+                var obj = new Object();
+                obj.OptionName = inputContent;
+                obj.SelectId = $(elm).parent(".item").attr("data-sid");
+                obj.TitleId = $(elm).parent(".item").attr("data-tid");
+                obj.OptionType = "2";
+                if (obj.SelectId == undefined) {
+                    obj.SelectId = "";
+                }
+                arrData.push(obj);
+            }
+        }
+    }
+    $("#PZData").val(JSON.stringify(arrData));
+
+    if (ReportIsGPS == false) {//没有开启定位
+        //当前位置处理
+        $("#ProvinceName").val($("select[name='Province']  option:selected").text());
+        $("#CityName").val($("select[name='City']  option:selected").text());
+        $("#CountyName").val($("select[name='County']  option:selected").text());
+    }
+
+    $("#FaProvinceName").val($("select[name='FaProvince']  option:selected").text());
+    $("#FaCityName").val($("select[name='FaCity']  option:selected").text());
+    $("#FaCountyName").val($("select[name='FaCounty']  option:selected").text());
 }
